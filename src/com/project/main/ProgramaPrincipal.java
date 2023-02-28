585643fb26c73597d926d1cc3d78a73bf698667a
Josep Albert Tortosa Peris <joseptortosa33@gmail.com>
2023-02-28 17:07:46 +0100

    primer commit

---
+++ src/com/project/main/ProgramaPrincipal.java
@@ -0,0 +1,12 @@
+package com.project.main;
+
+public class ProgramaPrincipal {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola món!");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author PORTATIL
+ *
+ */
+module Projecte {
+}
