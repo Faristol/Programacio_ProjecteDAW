f7793e28cccce898661bf26206d3a5242c17e42e
Josep Albert Tortosa Peris <joseptortosa33@gmail.com>
2023-03-02 16:14:34 +0100

    implementació de Serialize

--- src/com/project/main/Actors.java
+++ src/com/project/main/Actors.java
@@ -1,9 +1,38 @@
 package com.project.main;
 
-public class Actors {
+import java.io.Serializable;
 
-	public Actors() {
-		// TODO Auto-generated constructor stub
+public class Actors implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nomActor=null;
+	private String cognomsActor=null;
+	private String dataNaixementActor=null;
+	public String getNomActor() {
+		return nomActor;
 	}
+	public void setNomActor(String nomActor) {
+		this.nomActor = nomActor;
+	}
+	public String getCognomsActor() {
+		return cognomsActor;
+	}
+	public void setCognomsActor(String cognomsActor) {
+		this.cognomsActor = cognomsActor;
+	}
+	public String getDataNaixementActor() {
+		return dataNaixementActor;
+	}
+	public void setDataNaixementActor(String dataNaixementActor) {
+		this.dataNaixementActor = dataNaixementActor;
+	}
+	public Actors(String nomActor, String cognomsActor, String dataNaixementActor) {
+		super();
+		this.nomActor = nomActor;
+		this.cognomsActor = cognomsActor;
+		this.dataNaixementActor = dataNaixementActor;
+	}
+	
 
 }
--- src/com/project/main/Directors.java
+++ src/com/project/main/Directors.java
@@ -1,9 +1,44 @@
 package com.project.main;
 
-public class Directors {
+import java.io.Serializable;
 
-	public Directors() {
-		// TODO Auto-generated constructor stub
+public class Directors implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nomDirector = null;
+	private String cognomsDirector = null;
+	private String dataNaixementDirector = null;
+
+	public String getNomDirector() {
+		return nomDirector;
+	}
+
+	public void setNomDirector(String nomDirector) {
+		this.nomDirector = nomDirector;
+	}
+
+	public String getCognomsDirector() {
+		return cognomsDirector;
+	}
+
+	public void setCognomsDirector(String cognomsDirector) {
+		this.cognomsDirector = cognomsDirector;
+	}
+
+	public String getDataNaixementDirector() {
+		return dataNaixementDirector;
+	}
+
+	public void setDataNaixementDirector(String dataNaixementDirector) {
+		this.dataNaixementDirector = dataNaixementDirector;
+	}
+
+	public Directors(String nomDirector, String cognomsDirector, String dataNaixementDirector) {
+		super();
+		this.nomDirector = nomDirector;
+		this.cognomsDirector = cognomsDirector;
+		this.dataNaixementDirector = dataNaixementDirector;
 	}
 
 }
