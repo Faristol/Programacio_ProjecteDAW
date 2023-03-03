662e884554b937abf79adde03e7f3b6f00283e15
Adrian Barroso <byadrian05@gmail.com>
2023-03-03 13:13:49 +0100

    Creació Menú

--- src/com/project/main/Registre.java
+++ src/com/project/main/Registre.java
@@ -23,14 +23,14 @@ public class Registre {
 		String dataNaixement = comprovacioDataNaixement(entrada);
 		String rol = comprovacioRol(entrada);
 		String id = obtencioId();
-		// guardem la informació de l'usuari en el registre d'usuaris (infoUsuaris.txt)
+		// guardem la informaciï¿½ de l'usuari en el registre d'usuaris (infoUsuaris.txt)
 		guardarInformacioUsuari(nom, cognoms, correu, contrassenya, poblacio, rol, dataNaixement, id);
 		// guardar la contrassenya_usuari en contrassenyesUsuaris.txt
 		guardarContrassenyaUsuari(contrassenya, nom);
 		// ara creem l'objecte usuari i els seus fitxers aplicant el metode
 		Usuari user = new Usuari(nom, cognoms, correu, contrassenya, poblacio, dataNaixement, id);
 		user.creacioCarpetaFitxer();
-		// inmediatament portem a l'usuari a iniciar sessió
+		// inmediatament portem a l'usuari a iniciar sessiï¿½
 		IniciarSessio is = new IniciarSessio();
 		is.iniciarSessio();
 		
@@ -41,16 +41,16 @@ public class Registre {
 
 		boolean troba = false;
 		String nom = "";
-		// per a evitar desbordament de línia i que no genere un salt de pàgina fixem la
+		// per a evitar desbordament de lï¿½nia i que no genere un salt de pï¿½gina fixem la
 		// longitud en uns 30 per al nom i 40 pals cognoms, 60 pal correu, i 30 per a la
 		// poblacio
 		System.out.println("Introdueix el teu nom:");
 		do {
 			nom = entrada.nextLine().trim();
 			if (nom.contains(";")) {
-				System.out.println("No pots usar el símbol \";");
+				System.out.println("No pots usar el sï¿½mbol \";");
 			} else if (nom.length() < 1 || nom.length() > 30) {
-				System.out.println("La longitud del nom ha de tindre entre 1 i 20 caràcters. Torna a provar.");
+				System.out.println("La longitud del nom ha de tindre entre 1 i 20 carï¿½cters. Torna a provar.");
 			} else {
 				int comptadorOcurrencies = 0;
 
@@ -66,7 +66,7 @@ public class Registre {
 						troba = true;
 						lectorFitxer.close();
 					} else {
-						System.out.println("El nom introduït no està disponible. Torna a provar.");
+						System.out.println("El nom introduï¿½t no estï¿½ disponible. Torna a provar.");
 					}
 				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
@@ -88,9 +88,9 @@ public class Registre {
 		do {
 			cognom = entrada.nextLine().trim();
 			if (cognom.contains(";")) {
-				System.out.println("No pots usar el símbol \";");
+				System.out.println("No pots usar el sï¿½mbol \";");
 			} else if (cognom.length() < 1 || cognom.length() > 40) {
-				System.out.println("La longitud del cognom ha de tindre entre 1 i 40 caràcters.");
+				System.out.println("La longitud del cognom ha de tindre entre 1 i 40 carï¿½cters.");
 			} else {
 				int comptadorOcurrencies = 0;
 
@@ -106,7 +106,7 @@ public class Registre {
 						troba = true;
 						lectorFitxer.close();
 					} else {
-						System.out.println("El cognom introduït no està disponible. Torna a provar.");
+						System.out.println("El cognom introduï¿½t no estï¿½ disponible. Torna a provar.");
 						comptadorOcurrencies = 0;
 					}
 				} catch (FileNotFoundException e) {
@@ -130,9 +130,9 @@ public class Registre {
 			correu = entrada.nextLine().trim();
 			boolean coincideix = Pattern.compile("[\\w-\\.]+@[\\w-]+\\.[\\w]{2,4}").matcher(correu).matches();
 			if (coincideix == false) {
-				System.out.println("El correu introduït no presenta un format vàlid.");
+				System.out.println("El correu introduï¿½t no presenta un format vï¿½lid.");
 			} else if (correu.length() > 60) {
-				System.out.println("La longitud del correu no pot superar els 60 caràcters.");
+				System.out.println("La longitud del correu no pot superar els 60 carï¿½cters.");
 			} else {
 				int comptadorOcurrencies = 0;
 
@@ -148,7 +148,7 @@ public class Registre {
 						lectorFitxer.close();
 						troba = true;
 					} else {
-						System.out.println("El correu introduït no està disponible. Torna a provar.");
+						System.out.println("El correu introduï¿½t no estï¿½ disponible. Torna a provar.");
 					}
 				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
@@ -169,7 +169,7 @@ public class Registre {
 			contrassenya = entrada.nextLine().trim();
 			boolean valida = Pattern.compile("(?!.*;)[\\w!@#$%^&*()-+=]{6,20}").matcher(contrassenya).matches();
 			if (valida == false) {
-				System.out.println("La contrassenya ha de tindre entre 6 i 20 caràcters. No pots usar el símbol \";");
+				System.out.println("La contrassenya ha de tindre entre 6 i 20 carï¿½cters. No pots usar el sï¿½mbol \";");
 			} else {
 				System.out.println("Torna a introduir la contrassenya:");
 				String contrassenyaVerificacio = entrada.nextLine().trim();
@@ -189,13 +189,13 @@ public class Registre {
 		boolean troba = false;
 		String poblacio = null;
 		do {
-			System.out.println("Introdueix la població");
+			System.out.println("Introdueix la poblaciï¿½");
 			poblacio = entrada.nextLine().trim();
-			boolean valida = Pattern.compile("(?iu)[a-zà-ÿ\\s]+").matcher(poblacio).matches();
+			boolean valida = Pattern.compile("(?iu)[a-zï¿½-ï¿½\\s]+").matcher(poblacio).matches();
 			if (valida == false) {
-				System.out.println("Població no vàlida");
+				System.out.println("Poblaciï¿½ no vï¿½lida");
 			} else if (poblacio.length() < 0 || poblacio.length() > 30) {
-				System.out.println("La longitud de la poblacio no pot superar els 30 caràcters.");
+				System.out.println("La longitud de la poblacio no pot superar els 30 carï¿½cters.");
 			} else {
 				troba = true;
 			}
@@ -212,7 +212,7 @@ public class Registre {
 			boolean valida = Pattern.compile("([0-2][1-9]|[1-9]0|3[0-1])/(0[1-9]|1[0-2])/((19|20)\\d{2})")
 					.matcher(dataNaixement).matches();
 			if (valida == false) {
-				System.out.println("Data de naixement en format invàlid. El format ha de ser dd/mm/aaaa");
+				System.out.println("Data de naixement en format invï¿½lid. El format ha de ser dd/mm/aaaa");
 			} else {
 				troba = true;
 			}
