9af14bf6d1d71f3d0d04ae70c81af1f6910344e0
Josep Albert Tortosa Peris <joseptortosa33@gmail.com>
2023-03-03 20:05:04 +0100

    debug scanner

--- src/com/project/main/Accio.java
+++ src/com/project/main/Accio.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 
 public class Accio {
 	@SuppressWarnings("resource")
-	public void menuAccio(){
-		 Scanner entrada = new Scanner(System.in);
-		
+	public void menuAccio() {
+		Scanner entrada = new Scanner(System.in);
 		System.out.println();
 		System.out.println("-".repeat(43));
 		System.out.println("Què desitja fer?");
@@ -15,24 +14,25 @@ public class Accio {
 		System.out.println("2.Accedir a les teues llistes");
 		System.out.println("3.Fer enrere");
 		System.out.println("-".repeat(43));
-		
 
-		boolean troba = false;
+		int troba = 0;
 		int j = 0;
-		
+
 		do {
-	
-			if(!entrada.hasNextInt()) {
+
+			if (!entrada.hasNextInt()) {
 				System.out.println("Has d'introduir un nombre enter");
+				entrada.nextLine();
 			}
 			j = entrada.nextInt();
-			if (j == 1 || j == 2) {
-				troba = true;
+			if (j == 1 || j == 2 || j == 3) {
+				troba = 1;
 			} else {
-				System.out.println("Has de polsar 1 per a veurer les llistes generals, 2 per a veurer les teues llistes o 3 per a fer enrere.");
+				System.out.println(
+						"Has de polsar 1 per a veurer les llistes generals, 2 per a veurer les teues llistes o 3 per a fer enrere.");
 			}
-			entrada.nextLine();
-		} while (!troba);
+		} while (troba == 0);
+		System.out.println(j);
 
 	}
 }
